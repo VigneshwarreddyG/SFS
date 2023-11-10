830865f938a35d524bcdd250773a7e8f920fb6d5
Saicharan2707l <145713653+Saicharan2707l@users.noreply.github.com>
2023-11-10 17:51:24 -0600

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,38 +1,81 @@
+
 import unittest
-from app import app, mysql  # Import your Flask app and MySQL connection
-from flask import request
-import app  # Import your Flask app module
+from flask_testing import TestCase
+from app import app
 
-class AdminLoginTestCase(unittest.TestCase):
+class TestApp(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        return app
 
-    def setUp(self):
-        # Set up a test client and a testing environment
-        self.app = app.test_client()
-        self.app.testing = True
+    def test_index_route(self):
+        response = self.client.get('/')
+        self.assert200(response)
+        self.assert_template_used('main.html')
 
-    def tearDown(self):
-        # Clean up after the test
-        pass
+    def test_customer_register_route(self):
+        response = self.client.get('/Custregisterhtml')
+        self.assert200(response)
+        self.assert_template_used('Custregister.html')
 
-    def test_successful_login(self):
-        # Test a successful login
-        response = self.app.post('/adminlogin', data=dict(username='admin_user', password='admin_pass'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Logged in successfully', response.data)
+    def test_customer_login_route(self):
+        response = self.client.get('/Custloginhtml')
+        self.assert200(response)
+        self.assert_template_used('Custlogin.html')
 
-    def test_invalid_credentials(self):
-        # Test login with invalid credentials
-        response = self.app.post('/adminlogin', data=dict(username='invalid_user', password='invalid_pass'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Incorrect username / password', response.data)
+    def test_agent_login_route(self):
+        response = self.client.get('/Agentloginhtml')
+        self.assert200(response)
+        self.assert_template_used('agentlogin.html')
 
-    def test_account_type_check(self):
-        # Test login with a non-admin account
-        response = self.app.post('/adminlogin', data=dict(username='non_admin_user', password='non_admin_pass'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Please login as a Customer', response.data)
+    def test_agent_register_route(self):
+        response = self.client.get('/Ageregisterhtml')
+        self.assert200(response)
+        self.assert_template_used('Agentregister.html')
 
-    # Add more test cases as needed
+    def test_admin_login_route(self):
+        response = self.client.get('/Admin')
+        self.assert200(response)
+        self.assert_template_used('Adminlogin.html')
 
-if __name__ == '__main__':
+    def test_agent_dashboard_route(self):
+        response = self.client.get('/agentdashboard')
+        self.assert200(response)
+
+    def test_car_booking_route(self):
+        response = self.client.post('/Carbooking', data={'carModel': 'TestModel', 'paymentInfo': 'TestPayment', 'modelNumber': 'TestModelNumber', 'appointmentDate': '2023-01-01'})
+        print("::::::::::::::::::::::")
+        print(response)
+        self.assert200(response)
+        # Add assertions for the behavior of the Carbooking route based on the form data
+
+    def test_customer_login_valid_credentials(self):
+        response = self.client.post('/customerlogin', data={'username': 'valid_username', 'password': 'valid_password'})
+        self.assertStatus(response, 200)  # Check for a redirect status
+        response = self.client.get(response.headers['Location'])  # Follow the redirect
+        self.assert200(response)
+        self.assert_template_used('CustomerDashboard.html')
+
+    def test_customer_login_valid_credentials(self):
+        response = self.client.post('/customerlogin', data={'username': 'valid_username', 'password': 'valid_password'})
+        self.assertStatus(response, 302)  # Check for a redirect status
+        response = self.client.get(response.headers['Location'])  # Follow the redirect
+        self.assert200(response)
+        # Add more assertions based on the expected behavior after a successful login
+        self.assert_template_used('ExpectedTemplateAfterLogin.html')
+
+    def test_customer_register_valid_data(self):
+        response = self.client.post('/customerregister', data={'fname': 'John', 'lname': 'Doe', 'address': '123 Main St', 'username': 'john_doe', 'phone': '1234567890', 'email': 'john.doe@example.com', 'password': 'secure_password'})
+        self.assert200(response)
+        self.assert_template_used('Custlogin.html')
+        # Add more assertions based on the expected behavior when using valid registration data
+
+    def test_customer_register_existing_username(self):
+        # Ensure this username already exists in your database
+        response = self.client.post('/customerregister', data={'fname': 'John', 'lname': 'Doe', 'address': '123 Main St', 'username': 'existing_username', 'phone': '1234567890', 'email': 'john.doe@example.com', 'password': 'secure_password'})
+        self.assert200(response)
+        self.assert_template_used('Custregister.html')
+
+if __name__ == '_main_':
     unittest.main()
