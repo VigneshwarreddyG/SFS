ec17689f7120dec5ba50cede86a96304511a8070
Saicharan2707l <145713653+Saicharan2707l@users.noreply.github.com>
2023-12-08 23:34:57 -0600

    Update test_cutomerdashboard.py

--- test_cutomerdashboard.py
+++ test_cutomerdashboard.py
@@ -2,18 +2,20 @@ import pytest
 from flask import Flask
 from app import app  # Import your Flask app
 
+# Define a Pytest fixture to create a test client
 @pytest.fixture
 def client():
     client = app.test_client()
     return client
 
+# Test case to check the response status code for the admin dashboard route
 def test_admin_dashboard_response(client):
     response = client.get('/customer_dashboard')
-    print('hello')
-    assert response.status_code == 200
-# Testing the admin dash board containing the content in it
+    print('hello')  # Print a message for debugging or verification
+    assert response.status_code == 200  # Assert that the response status code is 200
+
+# Test case to check the content of the admin dashboard route
 def test_admin_dashboard_content(client):
     response = client.get('/customer_dashboard')
-    content = response.get_data(as_text=True)
-    assert "Welcome to the Customer Dashboard" in content
-    
+    content = response.get_data(as_text=True)  # Get the response content as text
+    assert "Welcome to the Customer Dashboard" in content  # Assert that a specific content is present in the response
